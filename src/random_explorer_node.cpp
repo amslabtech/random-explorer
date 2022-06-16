1e7d71bd4adc0583c65f80164aa1046964ad7906
YasunoriHirakawa <area51muepon1ichi@gmail.com>
2022-06-16 21:53:34 +0900

    Update launch for multiple LiDARs

--- src/random_explorer_node.cpp
+++ src/random_explorer_node.cpp
@@ -7,7 +7,7 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "random_explorer");
     ros::param::param<int>("~hz", param.hz, 10);
     ros::param::param<int>("~n_angle_samples", param.n_angle_samples, 360);
-    ros::param::param<int>("~n_direction_groups", param.n_direction_groups, 8);
+    ros::param::param<int>("~n_direction_groups", param.n_direction_groups, 16);
     ros::param::param<double>("~goal_torelance", param.goal_torelance, 3.0);
 
     random_explorer::RandomExplorer random_explorer(param);
